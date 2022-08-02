db5f970b5952d2850fc951642886a941e0586a1b
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2022-08-02 12:00:35 +0900

    fix target data

--- Informer/data/data_loader.py
+++ Informer/data/data_loader.py
@@ -157,6 +157,10 @@ class Dataset_BTC(Dataset):
 
         if self.option == 'pct':
             df_raw = add_features(df_raw, self.feature_add)[(self.feature_add-1):]
+        elif self.option == 'mean':
+            df_raw = add_features(df_raw, self.feature_add)[(self.feature_add - 1):]
+            df_raw['hi'] = df_raw['hi'].rolling(12).mean()
+            df_raw['lo'] = df_raw['lo'].rolling(12).mean()
         elif self.option == 'feature_engineering':
             df_raw['spread'] = (df_raw['hi'] - df_raw['lo']) + 10
             df_raw = add_features_v2(df_raw, self.feature_add)[(self.feature_add - 1):]
