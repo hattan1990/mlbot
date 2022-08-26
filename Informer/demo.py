b26ddd2f3c5fbbfa4fafbcdf32da86719aa783d6
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2022-08-26 11:09:58 +0900

    fix demo

--- Informer/demo.py
+++ Informer/demo.py
@@ -1,126 +1,46 @@
-import argparse
-import os
-import torch
-import sys
-
-from utils.tools import dotdict
 from exp.exp_informer import Exp_Informer
+from utils.tools import dotdict
+from config import args
+import numpy as np
 import torch
+import mlflow
 
-#モデルを指定します。
-args = dotdict()
-args.model = 'informer'
-
-#データセットとパスを指定
-args.data = 'ETTh1'
-args.root_path = '../ETDataset/ETT-small/'
-args.data_path = 'ETTh1.csv'
-
-#予測タスク、ターゲット(y)、時間フィーチャーエンコーディングを指定
-args.features = 'M'
-args.target = 'OT'
-args.freq = 'h' # h:hourly
-
-#トレーニング済みモデルを指定
-args.checkpoints = './informer_checkpoints'
-
-#EncoderとDecoderの入力するデータの長さを指定
-args.seq_len = 96
-args.label_len = 48
-args.pred_len = 24
-
-#EncoderとDecoderの入力バッチサイズを指定
-#モデルのレイア層、self-attentionのヘッド数、全結合層のノード数を指定
-args.enc_in = 7 # encoder input size
-args.dec_in = 7 # decoder input size
-args.c_out = 7 # output size
-args.factor = 5 # probsparse attn factor
-args.d_model = 512 # dimension of model
-args.n_heads = 8 # num of heads
-args.e_layers = 2 # num of encoder layers
-args.d_layers = 1 # num of decoder layers
-args.d_ff = 2048 # dimension of fcn in model
-args.dropout = 0.05 # dropout
-
-#デフォルトのパラメーター設定
-args.attn = 'prob' # attention used in encoder, options:[prob, full]
-args.embed = 'timeF' # time features encoding, options:[timeF, fixed, learned]
-args.activation = 'gelu' # activation
-args.distil = True # whether to use distilling in encoder
-args.output_attention = False # whether to output attention in ecoder
-args.mix = True
-args.padding = 0
-args.freq = 'h'
-
-#バッチサイズ、学習率、ロースファンクションなどを指定
-args.batch_size = 32
-args.learning_rate = 0.0001
-args.loss = 'mse'
-args.lradj = 'type1'
-args.use_amp = False # whether to use automatic mixed precision training
-
-#並列計算するかどうか、トレーニングepoch数を指定
-args.num_workers = 0
-args.itr = 1
-args.train_epochs = 6
-args.patience = 3
-args.des = 'exp'
-
-#GPUを使用するかどうかを指定
-args.use_gpu = True if torch.cuda.is_available() else False
-args.gpu = 0
-
-args.use_multi_gpu = False
-args.devices = '0,1,2,3'
-args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
-
-if args.use_gpu and args.use_multi_gpu:
-    args.devices = args.devices.replace(' ','')
-    device_ids = args.devices.split(',')
-    args.device_ids = [int(id_) for id_ in device_ids]
-    args.gpu = args.device_ids[0]
-
-#トレーニングデータセットのパラメータを設定
-data_parser = {
-    'ETTh1':{'data':'ETTh1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-    'ETTh2':{'data':'ETTh2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-    'ETTm1':{'data':'ETTm1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-    'ETTm2':{'data':'ETTm2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-}
-if args.data in data_parser.keys():
-    data_info = data_parser[args.data]
-    args.data_path = data_info['data']
-    args.target = data_info['T']
-    args.enc_in, args.dec_in, args.c_out = data_info[args.features]
-
-args.detail_freq = args.freq
-args.freq = args.freq[-1:]
+title = 'Predict hi&lo eth'
 
 def main(args):
     print('Args in experiment:')
     print(args)
+    client = mlflow.tracking.MlflowClient()
+    experiment = client.get_experiment_by_name(title)
+    if experiment is not None:
+        exp_id = experiment.experiment_id
+    else:
+        exp_id = client.create_experiment(title)
 
-    Exp = Exp_Informer
+    tag_list = ['model', 'data', 'root_path', 'data_path', 'features',
+                'target', 'target_num', 'freq']
+    with mlflow.start_run(experiment_id=exp_id):
+        #set tags
+        for key in args.keys():
+            if key in tag_list:
+                mlflow.set_tag(key=key, value=args[key])
+            elif key == 'runname':
+                mlflow.set_tag(key='mlflow.runName', value=args[key])
+            else:
+                mlflow.log_param(key, args[key])
 
-    for ii in range(args.itr):
-        # setting record of experiments
-        setting = '{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_at{}_fc{}_eb{}_dt{}_mx{}_{}_{}'.format(args.model, args.data, args.features,
-                    args.seq_len, args.label_len, args.pred_len,
-                    args.d_model, args.n_heads, args.e_layers, args.d_layers, args.d_ff, args.attn, args.factor,
-                    args.embed, args.distil, args.mix, args.des, ii)
 
-        exp = Exp(args) # set experiments
-        print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
-        exp.train(setting)
+        Exp = Exp_Informer
 
-        print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-        exp.test(setting)
+        for ii in range(args.itr):
+            # setting record of experiments
+            args.best_score = 0
+            exp = Exp(args) # set experiments
+            exp.test()
 
-        if args.do_predict:
-            print('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-            exp.predict(setting, True)
+            torch.cuda.empty_cache()
 
-        torch.cuda.empty_cache()
+    return
 
 if __name__ == '__main__':
     main(args)
