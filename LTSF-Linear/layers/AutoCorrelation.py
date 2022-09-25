bc7ef78afcf6e201ffe21e8d521e1cde14a7b99d
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2022-09-26 00:45:40 +0900

    fix  LTSF-Linear

--- LTSF-Linear/layers/AutoCorrelation.py
+++ LTSF-Linear/layers/AutoCorrelation.py
@@ -68,6 +68,9 @@ class AutoCorrelation(nn.Module):
         # aggregation
         tmp_values = values.repeat(1, 1, 1, 2)
         delays_agg = torch.zeros_like(values).float()
+        if torch.cuda.is_available():
+            init_index = init_index.cuda()
+
         for i in range(top_k):
             tmp_delay = init_index + delay[:, i].unsqueeze(1).unsqueeze(1).unsqueeze(1).repeat(1, head, channel, length)
             pattern = torch.gather(tmp_values, dim=-1, index=tmp_delay)
@@ -121,7 +124,11 @@ class AutoCorrelation(nn.Module):
         if self.training:
             V = self.time_delay_agg_training(values.permute(0, 2, 3, 1).contiguous(), corr).permute(0, 3, 1, 2)
         else:
-            V = self.time_delay_agg_inference(values.permute(0, 2, 3, 1).contiguous(), corr).permute(0, 3, 1, 2)
+            if torch.cuda.is_available():
+                V = self.time_delay_agg_inference(values.permute(0, 2, 3, 1).contiguous(), corr).permute(0, 3, 1, 2).cuda()
+            else:
+                V = self.time_delay_agg_inference(values.permute(0, 2, 3, 1).contiguous(), corr).permute(0, 3, 1, 2)
+
 
         if self.output_attention:
             return (V.contiguous(), corr.permute(0, 3, 1, 2))
