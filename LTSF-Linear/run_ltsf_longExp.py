b56ddde613b6432f7df99c3894f70cd8e8409b56
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2022-09-27 15:01:57 +0900

    fix config LTSF-Linear

--- LTSF-Linear/run_ltsf_longExp.py
+++ LTSF-Linear/run_ltsf_longExp.py
@@ -63,9 +63,9 @@ parser.add_argument('--do_predict', action='store_true', help='whether to predic
 # optimization
 parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
 parser.add_argument('--itr', type=int, default=2, help='experiments times')
-parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
-parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
-parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+parser.add_argument('--train_epochs', type=int, default=20, help='train epochs')
+parser.add_argument('--batch_size', type=int, default=320, help='batch size of train input data')
+parser.add_argument('--patience', type=int, default=5, help='early stopping patience')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='optimizer learning rate')
 parser.add_argument('--des', type=str, default='test', help='exp description')
 parser.add_argument('--loss', type=str, default='mse', help='loss function')
