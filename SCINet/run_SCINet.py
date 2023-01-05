6f769fd71ce6ca4b9e9c68891a1af9c2c28fce21
畠山剛 <stayhungry.stayfoolish.1990@gmail.com>
2023-01-06 00:01:45 +0900

    fix config  MM SCINet

--- SCINet/run_SCINet.py
+++ SCINet/run_SCINet.py
@@ -32,7 +32,7 @@ parser.add_argument('--devices', type=str, default='0',help='device ids of multi
 ### -------  input/output length settings --------------                                                                            
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of SCINet encoder, look back window')
 parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
-parser.add_argument('--pred_len', type=int, default=30, help='prediction sequence length, horizon')
+parser.add_argument('--pred_len', type=int, default=60, help='prediction sequence length, horizon')
 parser.add_argument('--concat_len', type=int, default=0)
 parser.add_argument('--single_step', type=int, default=0)
 parser.add_argument('--single_step_output_One', type=int, default=0)
