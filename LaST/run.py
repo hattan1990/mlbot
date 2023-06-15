5f3a2d7260b49f9b715996ae43d4fa2529d867de
hattan1990 <stayhungry.stayfoolish.1990@gmail.com>
2023-06-15 11:29:32 +0900

    Fix Config LaST

--- LaST/run.py
+++ LaST/run.py
@@ -32,9 +32,9 @@ parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple g
 parser.add_argument('--devices', type=str, default='0', help='device ids of multile gpus')
 
 # -------  input/output length settings --------------
-parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of encoder, look back window')
-parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
-parser.add_argument('--pred_len', type=int, default=60, help='prediction sequence length, horizon')
+parser.add_argument('--seq_len', type=int, default=240, help='input sequence length of encoder, look back window')
+parser.add_argument('--label_len', type=int, default=0, help='start token length of Informer decoder')
+parser.add_argument('--pred_len', type=int, default=120, help='prediction sequence length, horizon')
 
 # -------  model settings --------------
 parser.add_argument('--model', type=str, required=False, default='LaST', help='model of the experiment')
